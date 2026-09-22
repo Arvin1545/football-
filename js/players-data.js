f0ce9f675cb40dc936e8e3cbb3d62737d6b073f0
Arvin <arvin.hos92@gmail.com>
2026-09-22 12:36:05 +0330

    Create players-data.js

---
+++ js/players-data.js
@@ -0,0 +1,152 @@
+// players-data.js
+// داده‌های بازیکنان - ۵ تیم اول
+
+export const PLAYERS_DATA = {
+  
+  // 🏴󠁧󠁢󠁥󠁮󠁧󠁿 Manchester City
+  'Manchester City': [
+    { name: 'Ederson', pos: 'GK', ovr: 87, num: 31 },
+    { name: 'Stefan Ortega', pos: 'GK', ovr: 82, num: 18 },
+    { name: 'Ruben Dias', pos: 'CB', ovr: 88, num: 3 },
+    { name: 'Josko Gvardiol', pos: 'CB', ovr: 85, num: 24 },
+    { name: 'Abdukodir Khusanov', pos: 'CB', ovr: 78, num: 45 },
+    { name: 'Marc Guehi', pos: 'CB', ovr: 82, num: 6 },
+    { name: 'Rayan Ait-Nouri', pos: 'LB', ovr: 83, num: 21 },
+    { name: 'Rico Lewis', pos: 'RB', ovr: 81, num: 82 },
+    { name: 'Rodri', pos: 'CDM', ovr: 90, num: 16 },
+    { name: 'Mateo Kovacic', pos: 'CM', ovr: 84, num: 8 },
+    { name: 'Matheus Nunes', pos: 'CM', ovr: 82, num: 27 },
+    { name: 'Bernardo Silva', pos: 'CAM', ovr: 87, num: 20 },
+    { name: 'Phil Foden', pos: 'CAM', ovr: 88, num: 47 },
+    { name: 'Jeremy Doku', pos: 'LW', ovr: 84, num: 11 },
+    { name: 'Antoine Semenyo', pos: 'RW', ovr: 83, num: 22 },
+    { name: 'Savinho', pos: 'RW', ovr: 82, num: 26 },
+    { name: 'Erling Haaland', pos: 'ST', ovr: 91, num: 9 },
+    { name: 'Omar Marmoush', pos: 'ST', ovr: 84, num: 7 }
+  ],
+  
+  // 🏴󠁧󠁢󠁥󠁮󠁧󠁿 Liverpool
+  'Liverpool': [
+    { name: 'Alisson', pos: 'GK', ovr: 88, num: 1 },
+    { name: 'Giorgi Mamardashvili', pos: 'GK', ovr: 84, num: 25 },
+    { name: 'Vitezslav Jaros', pos: 'GK', ovr: 75, num: 62 },
+    { name: 'Virgil van Dijk', pos: 'CB', ovr: 88, num: 4 },
+    { name: 'Joe Gomez', pos: 'CB', ovr: 82, num: 2 },
+    { name: 'Ronald Araujo', pos: 'CB', ovr: 86, num: 5 },
+    { name: 'Milos Kerkez', pos: 'LB', ovr: 82, num: 6 },
+    { name: 'Kostas Tsimikas', pos: 'LB', ovr: 82, num: 21 },
+    { name: 'Jeremie Frimpong', pos: 'RB', ovr: 84, num: 30 },
+    { name: 'Conor Bradley', pos: 'RB', ovr: 80, num: 84 },
+    { name: 'Wataru Endo', pos: 'CDM', ovr: 81, num: 3 },
+    { name: 'Ryan Gravenberch', pos: 'CM', ovr: 85, num: 38 },
+    { name: 'Alexis Mac Allister', pos: 'CM', ovr: 86, num: 10 },
+    { name: 'Dominik Szoboszlai', pos: 'CAM', ovr: 85, num: 8 },
+    { name: 'Florian Wirtz', pos: 'CAM', ovr: 88, num: 7 },
+    { name: 'Federico Chiesa', pos: 'RW', ovr: 82, num: 14 },
+    { name: 'Cody Gakpo', pos: 'LW', ovr: 84, num: 18 },
+    { name: 'Bradley Barcola', pos: 'LW', ovr: 86, num: 11 },
+    { name: 'Alexander Isak', pos: 'ST', ovr: 87, num: 9 },
+    { name: 'Hugo Ekitike', pos: 'ST', ovr: 82, num: 22 }
+  ],
+  
+  // 🇪🇸 Real Madrid
+  'Real Madrid': [
+    { name: 'Thibaut Courtois', pos: 'GK', ovr: 89, num: 1 },
+    { name: 'Andriy Lunin', pos: 'GK', ovr: 83, num: 13 },
+    { name: 'Raul Asencio', pos: 'CB', ovr: 79, num: 35 },
+    { name: 'Eder Militao', pos: 'CB', ovr: 85, num: 3 },
+    { name: 'Dean Huijsen', pos: 'CB', ovr: 81, num: 24 },
+    { name: 'Antonio Rudiger', pos: 'CB', ovr: 87, num: 22 },
+    { name: 'Ibrahima Konate', pos: 'CB', ovr: 85, num: 15 },
+    { name: 'Trent Alexander-Arnold', pos: 'RB', ovr: 87, num: 66 },
+    { name: 'Denzel Dumfries', pos: 'RB', ovr: 84, num: 2 },
+    { name: 'Marc Cucurella', pos: 'LB', ovr: 83, num: 3 },
+    { name: 'Alvaro Carreras', pos: 'LB', ovr: 80, num: 18 },
+    { name: 'Ferland Mendy', pos: 'LB', ovr: 84, num: 23 },
+    { name: 'Jude Bellingham', pos: 'CAM', ovr: 90, num: 5 },
+    { name: 'Eduardo Camavinga', pos: 'CM', ovr: 86, num: 12 },
+    { name: 'Federico Valverde', pos: 'CM', ovr: 88, num: 15 },
+    { name: 'Aurelien Tchouameni', pos: 'CDM', ovr: 86, num: 14 },
+    { name: 'Arda Guler', pos: 'CAM', ovr: 83, num: 24 },
+    { name: 'Bernardo Silva', pos: 'CM', ovr: 87, num: 20 },
+    { name: 'Thiago', pos: 'CM', ovr: 79, num: 8 },
+    { name: 'Vinicius Jr', pos: 'LW', ovr: 90, num: 7 },
+    { name: 'Kylian Mbappe', pos: 'ST', ovr: 91, num: 9 },
+    { name: 'Rodrygo', pos: 'RW', ovr: 87, num: 11 },
+    { name: 'Endrick', pos: 'ST', ovr: 80, num: 16 },
+    { name: 'Christian Spey', pos: 'ST', ovr: 78, num: 27 },
+    { name: 'Brahim Diaz', pos: 'RW', ovr: 83, num: 21 },
+    { name: 'Yan Diomande', pos: 'LW', ovr: 78, num: 30 }
+  ],
+  
+  // 🇪🇸 Barcelona
+  'Barcelona': [
+    { name: 'Joan Garcia', pos: 'GK', ovr: 82, num: 1 },
+    { name: 'Wojciech Szczesny', pos: 'GK', ovr: 85, num: 25 },
+    { name: 'Dominik Livakovic', pos: 'GK', ovr: 82, num: 13 },
+    { name: 'Ronald Araujo', pos: 'CB', ovr: 86, num: 4 },
+    { name: 'Pau Cubarsi', pos: 'CB', ovr: 84, num: 2 },
+    { name: 'Eric Garcia', pos: 'CB', ovr: 82, num: 24 },
+    { name: 'Andreas Christensen', pos: 'CB', ovr: 82, num: 15 },
+    { name: 'Jules Kounde', pos: 'RB', ovr: 85, num: 23 },
+    { name: 'Joao Cancelo', pos: 'RB', ovr: 83, num: 22 },
+    { name: 'Gerard Martin', pos: 'LB', ovr: 78, num: 35 },
+    { name: 'Alejandro Balde', pos: 'LB', ovr: 84, num: 3 },
+    { name: 'Pedri', pos: 'CM', ovr: 88, num: 8 },
+    { name: 'Gavi', pos: 'CM', ovr: 85, num: 6 },
+    { name: 'Fermin Lopez', pos: 'CM', ovr: 82, num: 16 },
+    { name: 'Marc Bernal', pos: 'CDM', ovr: 78, num: 28 },
+    { name: 'Rodri', pos: 'CDM', ovr: 90, num: 4 },
+    { name: 'Dani Olmo', pos: 'CAM', ovr: 86, num: 20 },
+    { name: 'Xavi Espart', pos: 'CM', ovr: 77, num: 30 },
+    { name: 'Lamine Yamal', pos: 'RW', ovr: 89, num: 10 },
+    { name: 'Raphinha', pos: 'LW', ovr: 87, num: 11 },
+    { name: 'Karim Adeyemi', pos: 'LW', ovr: 84, num: 27 },
+    { name: 'Anthony Gordon', pos: 'LW', ovr: 85, num: 14 },
+    { name: 'Gabriel Jesus', pos: 'ST', ovr: 83, num: 9 },
+    { name: 'Bryan Gil', pos: 'LW', ovr: 80, num: 21 }
+  ],
+  
+  // 🇩🇪 Bayern Munich
+  'Bayern Munich': [
+    { name: 'Manuel Neuer', pos: 'GK', ovr: 87, num: 1 },
+    { name: 'Sven Ulreich', pos: 'GK', ovr: 78, num: 26 },
+    { name: 'Jonas Urbig', pos: 'GK', ovr: 75, num: 40 },
+    { name: 'Dayot Upamecano', pos: 'CB', ovr: 86, num: 2 },
+    { name: 'Kim Min-jae', pos: 'CB', ovr: 85, num: 3 },
+    { name: 'Jonathan Tah', pos: 'CB', ovr: 85, num: 4 },
+    { name: 'Hiroki Ito', pos: 'CB', ovr: 81, num: 21 },
+    { name: 'Alphonso Davies', pos: 'LB', ovr: 86, num: 19 },
+    { name: 'Nathaniel Brown', pos: 'LB', ovr: 78, num: 27 },
+    { name: 'Sacha Boey', pos: 'RB', ovr: 80, num: 23 },
+    { name: 'Josip Stanisic', pos: 'RB', ovr: 82, num: 44 },
+    { name: 'Joshua Kimmich', pos: 'CDM', ovr: 87, num: 6 },
+    { name: 'Aleksandar Pavlovic', pos: 'CDM', ovr: 82, num: 45 },
+    { name: 'Leon Goretzka', pos: 'CM', ovr: 85, num: 8 },
+    { name: 'Konrad Laimer', pos: 'CM', ovr: 84, num: 27 },
+    { name: 'Tom Bischof', pos: 'CM', ovr: 78, num: 30 },
+    { name: 'Jamal Musiala', pos: 'CAM', ovr: 89, num: 42 },
+    { name: 'Ismaila Sarr', pos: 'RW', ovr: 82, num: 26 },
+    { name: 'Serge Gnabry', pos: 'RW', ovr: 84, num: 7 },
+    { name: 'Michael Olise', pos: 'RW', ovr: 86, num: 17 },
+    { name: 'Luis Diaz', pos: 'LW', ovr: 86, num: 14 },
+    { name: 'Harry Kane', pos: 'ST', ovr: 90, num: 9 },
+    { name: 'Bastian Assomo', pos: 'ST', ovr: 78, num: 28 }
+  ]
+};
+
+// ====================================================
+// ترکیب پیش‌فرض (۱۱ نفره)
+// ====================================================
+export const DEFAULT_FORMATION = {
+  GK: 1,
+  CB: 2,
+  LB: 1,
+  RB: 1,
+  CDM: 1,
+  CM: 2,
+  CAM: 1,
+  LW: 1,
+  RW: 1,
+  ST: 1
+};
